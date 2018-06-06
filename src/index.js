3afbc1ecbf6f082706049a0e25f78d57b21a1f39
romaindso <romain.dso@gmail.com>
2018-06-06 11:56:30 +0200

    setup redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import App from './App';
-import './app.scss';
+import { createStore } from "redux";
+import reducer from "./reducers/index";
+import App from "./views/App";
+import "./app.scss";
 
-document.addEventListener("DOMContentLoaded", function () {
-  new App();
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+console.log(store.getState());
+
+const app = new App(store);
+document.addEventListener("DOMContentLoaded", function() {
+  app.render();
 });
